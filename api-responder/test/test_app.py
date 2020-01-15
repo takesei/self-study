1d84de7e869ccf3bc194ca75b02163899f3baa11
takesei <sei06k14@gmail.com>
2020-01-16 01:59:33 +0900

    :rotating_light: Update test

--- api-responder/test/test_app.py
+++ api-responder/test/test_app.py
@@ -8,3 +8,8 @@ def api():
 def test_root(api):
     r = api.requests.get('/')
     assert r.text == 'hello, world!'
+
+def test_hello(api):
+    arg = 'honya'
+    r = api.requests.get(f'/hello/{arg}')
+    assert r.text == f'hello, {arg}!'
