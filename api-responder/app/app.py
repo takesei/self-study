c0be74bd36da4bf92378058ea351cc25a00cdb6f
takesei <sei06k14@gmail.com>
2020-01-16 02:16:54 +0900

    :sparkles: Add Json response feature

--- api-responder/app/app.py
+++ api-responder/app/app.py
@@ -7,8 +7,12 @@ def root(req, resp):
     resp.text = 'hello, world!'
 
 @api.route('/hello/{who}')
-def root(req, resp, *, who):
+def hello(req, resp, *, who):
     resp.text = f'hello, {who}!'
+
+@api.route('/hello/{who}/json')
+def hello_json(req, resp, *, who):
+    resp.media = {'message': f'hello, {who}'}
 
 if __name__ == '__main__':
     api.run()
